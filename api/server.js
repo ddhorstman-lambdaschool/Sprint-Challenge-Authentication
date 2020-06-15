f9a2482860865360826fd147f26d05d1cfb9765a
David Horstman <davidzilla27@gmail.com>
2020-06-14 21:00:39 -0400

    Initial session configuration

--- api/server.js
+++ api/server.js
@@ -1,6 +1,32 @@
 const express = require('express');
 const cors = require('cors');
 const helmet = require('helmet');
+const secrets = require('../config/secrets');
+
+const session = require("express-session");
+const knexSessionStore = require("connect-session-knex")(session);
+
+const sessionDuration = 1000 * 60 * 60 * 24;
+
+const sessionConfig = {
+  name: "node-auth1-session",
+  secret: secrets.SESSION_SECRET,
+  cookie: {
+    maxAge: sessionDuration,
+    secure: process.env.NODE_ENV === "production",
+    httpOnly: true,
+  },
+  resave: false,
+  saveUninitialized: false,
+
+  store: new knexSessionStore({
+    knex: require("../database/dbConfig"),
+    tableName: "session",
+    sidfieldname: "sid",
+    createtable: true,
+    clearInterval: sessionDuration,
+  }),
+};
 
 const authenticate = require('../auth/authenticate-middleware.js');
 const authRouter = require('../auth/auth-router.js');
@@ -9,8 +35,9 @@ const jokesRouter = require('../jokes/jokes-router.js');
 const server = express();
 
 server.use(helmet());
-server.use(cors());
+server.use(cors({ origin: true, credentials: true}));
 server.use(express.json());
+server.use(session(sessionConfig));
 
 server.use('/api/auth', authRouter);
 server.use('/api/jokes', authenticate, jokesRouter);
---
+++ config/secrets.js
@@ -0,0 +1,4 @@
+module.exports = {
+  SESSION_SECRET:
+    "the magenta orangutan persisted on a diet of ironic dichromaticism",
+};
