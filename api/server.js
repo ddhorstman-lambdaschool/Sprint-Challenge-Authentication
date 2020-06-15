149e54d79160b9e2ffb9ac3e0e5c3fe9f9f5e050
David Horstman <davidzilla27@gmail.com>
2020-06-14 21:13:47 -0400

    Implement authentication

--- api/server.js
+++ api/server.js
@@ -31,6 +31,8 @@ const sessionConfig = {
 const authenticate = require('../auth/authenticate-middleware.js');
 const authRouter = require('../auth/auth-router.js');
 const jokesRouter = require('../jokes/jokes-router.js');
+const { custom404, errorHandling } = require("../config/errors");
+
 
 const server = express();
 
@@ -41,5 +43,8 @@ server.use(session(sessionConfig));
 
 server.use('/api/auth', authRouter);
 server.use('/api/jokes', authenticate, jokesRouter);
+
+server.all("*", custom404);
+server.use(errorHandling);
 
 module.exports = server;
--- auth/authenticate-middleware.js
+++ auth/authenticate-middleware.js
@@ -1,8 +1,5 @@
-/* 
-  complete the middleware code to check if the user is logged in
-  before granting access to the next middleware/route handler
-*/
-
-module.exports = (req, res, next) => {
-  res.status(401).json({ you: 'shall not pass!' });
+module.exports = function restrictAccess(req, res, next) {
+  req.session && req.session.user
+    ? next()
+    : res.status(401).json({ you: "shall not pass!" });
 };
