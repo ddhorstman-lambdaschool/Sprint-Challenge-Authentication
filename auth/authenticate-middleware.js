4d13c7566864579ce004d11aa2e5b190e147d445
David Horstman <davidzilla27@gmail.com>
2020-06-15 22:20:33 -0400

    Continue chasing session bug

--- auth/authenticate-middleware.js
+++ auth/authenticate-middleware.js
@@ -1,8 +1,11 @@
 const config = require("../config/serverInfo");
 module.exports = function restrictAccess(req, res, next) {
-  if (req.session && req.session.user) return next();
-
-  if (req.session) req.session.destroy();
-  res.clearCookie(config.COOKIE_NAME);
-  res.status(401).json({ message: "Error: you must be logged in to do that." });
+  if (req.session && req.session.user) next();
+  else {
+    if (req.session) req.session.destroy();
+    res.clearCookie(config.COOKIE_NAME);
+    res
+      .status(401)
+      .json({ message: "Error: you must be logged in to do that." });
+  }
 };
