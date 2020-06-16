8527f9aef86c62fb5f3038058f52509a2ada1f2d
David Horstman <davidzilla27@gmail.com>
2020-06-15 22:18:13 -0400

    Attempt to address session destruction bug

--- auth/authenticate-middleware.js
+++ auth/authenticate-middleware.js
@@ -2,7 +2,7 @@ const config = require("../config/serverInfo");
 module.exports = function restrictAccess(req, res, next) {
   if (req.session && req.session.user) return next();
 
+  if (req.session) req.session.destroy();
   res.clearCookie(config.COOKIE_NAME);
-  req.session.destroy();
   res.status(401).json({ message: "Error: you must be logged in to do that." });
 };
